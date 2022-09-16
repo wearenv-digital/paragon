f38a5176eda55153b9b3a336519b7f97a73eb8b2
wearenv-digital <design@wearenv.co.uk>
2022-09-16 22:35:57 +0100

    im so tired but its nearly there

--- server/db.js
+++ server/db.js
@@ -7,12 +7,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// dont touch this IT WORKS
+// Retrieve rows from the various databases depending on
+// product code
+
+// gets the list of features from cam_features
 
 async function getFeatures(req) {
-	var connection = await util.promisify(
-		mysqlConf.getConnection.bind(mysqlConf)
-	)();
+	var connection = await util.promisify(mysqlConf.getConnection.bind(mysqlConf))();
 	console.log(`connected as id ${connection.threadId}`);
 	var rows = await util.promisify(connection.query.bind(connection))(
 		'SELECT * FROM cam_features WHERE product_code = ?',
@@ -22,20 +23,90 @@ async function getFeatures(req) {
 	var result;
 	Object.keys(rows).forEach(function (key) {
 		result = rows[key];
-		console.log(result);
+		// console.log(result);
 	});
 	return result;
 }
 
-// This gets the row from
+// This gets the row from cam_info
 
 async function getInfo(req) {
-	var connection = await util.promisify(
-		mysqlConf.getConnection.bind(mysqlConf)
-	)();
+	var connection = await util.promisify(mysqlConf.getConnection.bind(mysqlConf))();
 	console.log(`connected as id ${connection.threadId}`);
 	var rows = await util.promisify(connection.query.bind(connection))(
-		'SELECT * FROM camera_info WHERE product_code = ?',
+		'SELECT * FROM cam_info WHERE product_code = ?',
+		[req.params.product_code]
+	);
+	connection.release();
+	var result;
+	Object.keys(rows).forEach(function (key) {
+		result = rows[key];
+		// console.log(result);
+	});
+	return result;
+}
+
+// This gets the row from cam_specs
+
+async function getCamSpecs(req) {
+	var connection = await util.promisify(mysqlConf.getConnection.bind(mysqlConf))();
+	console.log(`connected as id ${connection.threadId}`);
+	var rows = await util.promisify(connection.query.bind(connection))(
+		'SELECT * FROM cam_specs WHERE product_code = ?',
+		[req.params.product_code]
+	);
+	connection.release();
+	var result;
+	Object.keys(rows).forEach(function (key) {
+		result = rows[key];
+		// console.log(result);
+	});
+	return result;
+}
+
+// This gets the row from audio_video
+
+async function getAudioVideo(req) {
+	var connection = await util.promisify(mysqlConf.getConnection.bind(mysqlConf))();
+	console.log(`connected as id ${connection.threadId}`);
+	var rows = await util.promisify(connection.query.bind(connection))(
+		'SELECT * FROM audio_video WHERE product_code = ?',
+		[req.params.product_code]
+	);
+	connection.release();
+	var result;
+	Object.keys(rows).forEach(function (key) {
+		result = rows[key];
+		// console.log(result);
+	});
+	return result;
+}
+
+// This gets the row from automation
+
+async function getAutomation(req) {
+	var connection = await util.promisify(mysqlConf.getConnection.bind(mysqlConf))();
+	console.log(`connected as id ${connection.threadId}`);
+	var rows = await util.promisify(connection.query.bind(connection))(
+		'SELECT * FROM automation WHERE product_code = ?',
+		[req.params.product_code]
+	);
+	connection.release();
+	var result;
+	Object.keys(rows).forEach(function (key) {
+		result = rows[key];
+		// console.log(result);
+	});
+	return result;
+}
+
+// This gets the row from elctrical_physical
+
+async function getElecPhys(req) {
+	var connection = await util.promisify(mysqlConf.getConnection.bind(mysqlConf))();
+	console.log(`connected as id ${connection.threadId}`);
+	var rows = await util.promisify(connection.query.bind(connection))(
+		'SELECT * FROM electrical_physical WHERE product_code = ?',
 		[req.params.product_code]
 	);
 	connection.release();
@@ -49,3 +120,7 @@ async function getInfo(req) {
 
 exports.getInfo = getInfo;
 exports.getFeatures = getFeatures;
+exports.getCamSpecs = getCamSpecs;
+exports.getAudioVideo = getAudioVideo;
+exports.getAutomation = getAutomation;
+exports.getElecPhys = getElecPhys;
--- server/routes.js
+++ server/routes.js
@@ -2,14 +2,22 @@ var express = require('express');
 const { appendFile } = require('fs');
 var router = express.Router();
 const path = require('path');
-const { getInfo, getFeatures } = require('./db');
-var mysqlConf = require('./sqlconfig.js').mysql_pool;
+const {
+	getInfo,
+	getFeatures,
+	getCamSpecs,
+	getAudioVideo,
+	getAutomation,
+	getElecPhys
+} = require('./db');
 
 // middleware for router
 router.use(function timeLog(req, res, next) {
 	console.log('Time: ', Date.now());
 	next();
 });
+
+// anything helpful we need for operation
 
 // routes
 
@@ -21,15 +29,48 @@ router.get('/product-page-template-copy', (req, res) => {
 	res.sendFile(path.resolve('../public/product-page-template-copy.html'));
 });
 
+// main product-page route
+
+function removeEmpty(obj) {
+	return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v != null));
+}
+
 router.get('/product-page/:product_code', async function (req, res) {
 	var camInfo = await getInfo(req);
 	var camFeatures = await getFeatures(req);
+	var camSpecs = await getCamSpecs(req);
+	var audioVideo = await getAudioVideo(req);
+	var automation = await getAutomation(req);
+	var elecPhys = await getElecPhys(req);
+
 	var featuresArray = Object.values(camFeatures);
+
+	// filter and mutate the array to omit any empties and the first element
 	featuresArray = featuresArray.filter((item) => !item.length < 1);
+	featuresArray = featuresArray.slice(1, featuresArray.length);
 	camInfo.featuresList = featuresArray;
+	var fullListObj = {};
+	fullListObj = {
+		...camInfo,
+		...camSpecs,
+		...audioVideo,
+		...automation,
+		...elecPhys
+	};
+	var keys;
+	keys = Object.keys(fullListObj).filter(
+		(k) =>
+			fullListObj[k] === 'n/a' || fullListObj[k] === '*' || fullListObj[k] === ''
+	);
+
+	console.log(keys);
+
+	// console.log(typeof keys);
+
+	// delete fullListObj
 	// res.send(camInfo);
-	res.render('test-view', {
-		camInfo: camInfo
+	res.render('product-page', {
+		camInfo: fullListObj
 	});
 });
 
