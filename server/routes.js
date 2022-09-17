96c5790729d2c8c297f91b5707f91a69170c8a0d
wearenv-digital <design@wearenv.co.uk>
2022-09-17 15:55:41 +0100

    turning everything into reuseable functions
    so the code ran fine, but i would have to specify each
    key.value for every line, this is not suitable when it comes to the
    next categories, or when some values do not exist, or have a value of
    n/a or *
    
    The object is made like this
    
    object = {
    	key.value,
    	key2.value
    }
    
    eg this would be
    {
    	illumination: value,
    	lens: value,
    	...
    	all the way down to
    	power: value,
    	op_temp: value
    }
    
    This would be fine, if, we were not separating into categories. I want
    (and finance wants) this to be automated, to minimise general grunt time
    for myself, so I am spending the time to separate and automate
    filtering of unwanted 'n/a' or '*' values, for each category.

--- server/routes.js
+++ server/routes.js
@@ -43,13 +43,28 @@ router.get('/product-page/:product_code', async function (req, res) {
 	var automation = await getAutomation(req);
 	var elecPhys = await getElecPhys(req);
 
+	function removeCode(arr, obj, name) {
+		arr = arr.filter((item) => !item.length < 1);
+		arr = arr.slice(1, arr.length);
+		obj.name = arr;
+		return obj;
+	}
 	var featuresArray = Object.values(camFeatures);
+	var specsArray = Object.values(camSpecs);
+	var AVArray = Object.values(audioVideo);
+	var autoArray = Object.values(automation);
+	var elecPhysArray = object.values(elecPhys);
+
 
 	// filter and mutate the array to omit any empties and the first element
 	featuresArray = featuresArray.filter((item) => !item.length < 1);
 	featuresArray = featuresArray.slice(1, featuresArray.length);
 	camInfo.featuresList = featuresArray;
+
+	
 	// camInfo is now the full list
+	
+	
 	var fullListObj = {};
 	fullListObj = {
 		...camInfo,
@@ -60,24 +75,94 @@ router.get('/product-page/:product_code', async function (req, res) {
 	};
 	var deadKeys;
 	var allKeys;
+
 	// fullListObj is now the 'wanted' object
 
 	// create array of deadKeys to filter out
-	deadKeys = Object.keys(fullListObj).filter(
-		(k) =>
-			fullListObj[k] === 'n/a' || fullListObj[k] === '*' || fullListObj[k] === ''
-	);
+
+	// turn the following into a reuasable function to perform on every object
+
+	function filterDead(obj) {
+		deadKeys = Object.keys(obj).filter(
+			(k) => obj[k] === 'n/a' || obj[k] === '*' || obj[k] === ''
+		);
+		return deadKeys;
+	}
+
+	// deadKeys = Object.keys(fullListObj).filter(
+	// 	(k) =>
+	// 		fullListObj[k] === 'n/a' || fullListObj[k] === '*' || fullListObj[k] === ''
+	// );
+
 	// create list of keys from every pair in the data
 
-	allKeys = Object.keys(fullListObj);
-	
-	// filter out deadKeys from all allkeys
-	let newKeys = [];
+	function listAllKeys(obj) {
+		allKeys = Object.keys(obj);
+		return allKeys;
+	}
 
-	newKeys = allKeys.reduce(function (prev, value) {
+	function checkDupes(allkeys, deadKeys) {
+		let newKeys;
+		newKeys = allKeys.reduce(function (prev, value) {
+			var isDuplicate = false;
+			for (var i = 0; i < deadKeys.length; i++) {
+				if (value === deadKeys[i]) {
+					isDuplicate = true;
+					break;
+				}
+			}
+			if (!isDuplicate) {
+				prev.push(value);
+			}
+			return prev;
+		}, []);
+	}
+
+	var deadInfoKeys;
+	var deadSpecsKeys;
+	var deadAVKeys;
+	var deadAutomationKeys;
+	var deadElecPhysKeys;
+
+	deadInfoKeys = filterDead(camInfo);
+	deadSpecsKeys = filterDead(camSpecs);
+	deadAVKeys = filterDead(audioVideo);
+	deadAutomationKeys = filterDead(automation);
+	deadElecPhysKeys = filterDead(elecPhys);
+
+	// console.log(deadAVKeys); //  works
+
+	// console.log(deadAutomationKeys); // works
+
+	// goodInfoKeys = checkDuplicates();
+
+	var allInfoKeys;
+	var allSpecsKeys;
+	var allAVKeys;
+	var allAutoKeys;
+	var allElecPhysKeys;
+
+	allInfoKeys = listAllKeys(camInfo);
+	allSpecsKeys = listAllKeys(camSpecs);
+	allAVKeys = listAllKeys(audioVideo);
+	allAutoKeys = listAllKeys(automation);
+	allElecPhysKeys = listAllKeys(elecPhys);
+	// console.log('allInfoKeys= ' + allInfoKeys);
+	// console.log('all elec keys = ' + allElecPhysKeys);
+
+	var newInfoKeys = [];
+	var newSpecsKeys = [];
+	var newAVKeys = [];
+	var newAutoKeys = [];
+	var newElecPhysKeys = [];
+
+	// The next function expressions return a list of goodkeys to create
+	// the final objects from.
+
+	newInfoKeys = allInfoKeys.reduce(function (prev, value) {
 		var isDuplicate = false;
-		for (var i = 0; i < deadKeys.length; i++) {
-			if (value === deadKeys[i]) {
+		for (var i = 0; i < deadInfoKeys.length; i++) {
+			if (value === deadInfoKeys[i]) {
 				isDuplicate = true;
 				break;
 			}
@@ -88,21 +173,110 @@ router.get('/product-page/:product_code', async function (req, res) {
 		return prev;
 	}, []);
 
-	// console.log(newKeys);
-	// finally create new object, with pairs from list of filtered keys 
+	newSpecsKeys = allSpecsKeys.reduce(function (prev, value) {
+		var isDuplicate = false;
+		for (var i = 0; i < deadSpecsKeys.length; i++) {
+			if (value === deadSpecsKeys[i]) {
+				isDuplicate = true;
+				break;
+			}
+		}
+		if (!isDuplicate) {
+			prev.push(value);
+		}
+		return prev;
+	}, []);
+
+	newAVKeys = allAVKeys.reduce(function (prev, value) {
+		var isDuplicate = false;
+		for (var i = 0; i < deadAVKeys.length; i++) {
+			if (value === deadAVKeys[i]) {
+				isDuplicate = true;
+				break;
+			}
+		}
+		if (!isDuplicate) {
+			prev.push(value);
+		}
+		return prev;
+	}, []);
+
+	newAutoKeys = allAutoKeys.reduce(function (prev, value) {
+		var isDuplicate = false;
+		for (var i = 0; i < deadAutomationKeys.length; i++) {
+			if (value === deadAutomationKeys[i]) {
+				isDuplicate = true;
+				break;
+			}
+		}
+		if (!isDuplicate) {
+			prev.push(value);
+		}
+		return prev;
+	}, []);
+
+	newElecPhysKeys = allElecPhysKeys.reduce(function (prev, value) {
+		var isDuplicate = false;
+		for (var i = 0; i < deadElecPhysKeys.length; i++) {
+			if (value === deadElecPhysKeys[i]) {
+				isDuplicate = true;
+				break;
+			}
+		}
+		if (!isDuplicate) {
+			prev.push(value);
+		}
+		return prev;
+	}, []);
+
+	// console.log(newElecPhysKeys);
 
 	var finalObj;
+	var finalInfo;
+	var finalSpecs;
+	var finalAV;
+	var finalAutomation;
+	var finalElecPhys;
+
+	// create the final object from each category to send
 	function select(arr, obj) {
 		let finalObj = {};
 		for (let k in obj) if (arr.includes(k)) finalObj[k] = obj[k];
 		return finalObj;
 	}
-	finalObj = select(newKeys, fullListObj);
+
+	finalInfo = select(newInfoKeys, camInfo);
+	finalSpecs = select(newSpecsKeys, camSpecs);
+	finalAV = select(newAVKeys, audioVideo);
+	finalAutomation = select(newAutoKeys, automation);
+	finalElecPhys = select(newElecPhysKeys, elecPhys);
+
+	// specsArray = Object.values(finalSpecs);
+	// specsArray = specsArray.filter((item) => !item.length < 1);
+	// specsArray = specsArray.slice(1, specsArray.length);
+	// finalSpecs.specsList = specsArray;
+
+	console.log(finalSpecs);
+	var finalObj;
+	finalObj = {
+		...finalInfo,
+		...finalSpecs,
+		...finalAV,
+		...finalAutomation,
+		...finalElecPhys
+	};
+
+	finalObj.info = finalInfo;
+	finalObj.specs = finalSpecs;
+	finalObj.AV = finalAV;
+	finalObj.automation = finalAutomation;
+	finalObj.elecPhys = finalElecPhys;
+	// finalObj = select(newKeys, fullListObj);
 	// send the finalObject to the view for rendering and processing
-	// res.send(camInfo);
-	res.render('product-page', {
-		camInfo: finalObj
-	});
+	res.send(finalObj);
+	// res.render('product-page', {
+	// 	camInfo: finalObj
+	// });
 });
 
 module.exports = router;
