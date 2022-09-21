97171743be34840ad2add61ae5a31e5340e1cf18
wearenv-digital <design@wearenv.co.uk>
2022-09-21 15:03:44 +0100

    ss

--- src/libs/dead-work.js
+++ src/libs/dead-work.js
@@ -19,22 +19,19 @@
 // 	}, []);
 // }
 
+// newKeys = allKeys.reduce(function (prev, value) {
+// 	var isDuplicate = false;
+// 	for (var i = 0; i < deadKeys.length; i++) {
+// 		if (value === deadKeys[i]) {
+// 			isDuplicate = true;
+// 			break;
+// 		}
+// 	}
+// 	if (!isDuplicate) {
+// 		prev.push(value);
+// 	}
+// 	return prev;
+// }, []);
 
-	// newKeys = allKeys.reduce(function (prev, value) {
-	// 	var isDuplicate = false;
-	// 	for (var i = 0; i < deadKeys.length; i++) {
-	// 		if (value === deadKeys[i]) {
-	// 			isDuplicate = true;
-	// 			break;
-	// 		}
-	// 	}
-	// 	if (!isDuplicate) {
-	// 		prev.push(value);
-	// 	}
-	// 	return prev;
-	// }, []);
-
-	// console.log(newKeys);
-	// finally create new object, with pairs from list of filtered keys
-
-
+// console.log(newKeys);
+// finally create new object, with pairs from list of filtered keys
